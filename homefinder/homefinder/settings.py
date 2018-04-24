5bde37c14d6d1e5b9dfd791316eda425827145b6
Nishant2050 <38308057+Nishant2050@users.noreply.github.com>
2018-04-24 11:10:34 +0530

    Update settings.py

--- homefinder/homefinder/settings.py
+++ homefinder/homefinder/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/2.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = ')t3wr*j3f%jxuwen&%ye&00e#r&swcczh8&p#cq4kx^$vvmc0v'
+SECRET_KEY = *****************************************************'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
